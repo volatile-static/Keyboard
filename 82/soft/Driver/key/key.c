25b37e8f614855dbd6d3e00911dfe3fb15e65d14
PasCal++ <59945270+volatile-static@users.noreply.github.com>
2020-02-12 08:41:17 +0800

    keys
    identity

--- 82/soft/Driver/key/key.c
+++ 82/soft/Driver/key/key.c
@@ -9,7 +9,7 @@ void key_init(void) {
 		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 | (GPIO_Pin_8 - GPIO_Pin_3);
 		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 
 		GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; 
-		GPIO_Init(GPIOB, &GPIO_InitStructure);
+		GPIO_Init(GPIOB, &GPIO_InitStructure); 
 	
 		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 - GPIO_Pin_0;
 		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
